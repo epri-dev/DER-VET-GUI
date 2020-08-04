2061828a91f7ffdd0868e517bb44f24e0fffde46
phna001 <hnathwani@epri.com>
2020-08-03 17:37:14 -0700

    basically completed HTML for new project page; css isnt rendering on my computer

--- DervetApp/src/renderer/components/Home/NewProject.vue
+++ DervetApp/src/renderer/components/Home/NewProject.vue
@@ -1,31 +1,41 @@
 <template>
-  <div id="wrapper">
-    <div class="form-horizontal form-buffer">
-<!--        @if (!ViewData.ModelState.IsValid)
-        {
-            <div class="form-group">
-                @Html.BootstrapAlert(Html.ValidationSummary(false))
-            </div>
-        } -->
-        <div class="form-group">
-            <label for="project">Name of the project:</label>
-            <input type="text" class="form-control" id="project">
-
-        </div>
-        <div class="form-group">
-
-            <div class="col-md-5">
-                <p class="tool-tip">Wizard projects guide you through constructing an analysis by asking a series of questions. Expert Mode projects allow you to directly edit the project configuration data used by DER-VET. Please note that this decision is final &mdash; once you select a Project Type you cannot change it.</p>
-            </div>
-        </div>
+    <div class="container body-content">
+        <h3>Create a New Project</h3>
         <hr />
-        <div class="form-group form-buffer">
-            <div class="col-md-12">
-                <button type="submit" class="btn btn-primary pull-right">Save and Continue</button>
+        <div class="form-horizontal form-buffer">
+            <div class="form-group">
+                <div class="col-md-3 text-right">
+                    <label class="control-label" for="ProjectName">Project Name</label>
+                </div>
+                <div class="col-md-4">
+                    <input type="text" class="form-control" id="ProjectName">
+                </div>
+                <div class="col-md-5">
+                    <p type="tool-tip">Name of the project.</p>
+                </div>
+            </div>
+            <div class="form-group">
+                <!-- <div class="col-md-3 text-right">
+                    <label class="control-label" for="ProjectType">Project Type</label>
+                </div> -->
+                <div class="col-md-4">
+                    <b-dropdown id="ProjectType" text="Project Type">
+                        <b-dropdown-item>Exert</b-dropdown-item>
+                        <b-dropdown-item>Wizard</b-dropdown-item>
+                    </b-dropdown>
+                </div>
+                <div class="col-md-5">
+                    <p class="tool-tip">Wizard projects guide you through constructing an analysis by asking a series of questions. Expert Mode projects allow you to directly edit the project configuration data used by DER-VET. Please note that this decision is final &mdash; once you select a Project Type you cannot change it.</p>
+                </div>
+            </div>
+            <hr />
+            <div class="form-group form-buffer">
+                <div class="col-md-12">
+                    <button type="submit" class="btn btn-primary pull-right">Save and Continue</button>
+                </div>
             </div>
         </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -33,3 +43,14 @@
     name: 'newProject',
   };
 </script>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+  
+  .form-horizontal .control-label {
+    text-align: left;
+    padding-top: 3px;
+    padding-right: 0;
+}
+</style>
+
---
+++ DervetApp/src/renderer/components/Home/ProjectImported.vue
@@ -0,0 +1,32 @@
+﻿@model DerivetUI.Web.Models.Home.HomeProjectImportedViewModel
+
+@{
+    ViewBag.Title = "DER-VET: Project Imported!";
+}
+
+<h2>Project Imported!</h2>
+<hr />
+
+<p>
+    The project <b>@Model.Project.ProjectName</b> has been successfully imported.
+    
+    @if (Model.ResultData != null)
+    {
+        <text>This results for this project were also imported.</text>
+    }
+</p>
+<p class="buffer-top">
+    @if (Model.Project.ExpertMode)
+    {
+        @Html.ActionLink("Open Imported Project", "Edit", "ExpertMode", new { id = Model.Project.ProjectID }, new { @class = "btn btn-warning" })
+    }
+    else
+    {
+        @Html.ActionLink("Open Imported Project", "StartProject", "Wizard", null, new { @class = "btn btn-warning" })
+    }
+
+    @if (Model.ResultData != null)
+    {
+        @Html.ActionLink("View Project Results", "Results", "Wizard", new { id = Model.Project.ProjectID, rid = Model.ResultData.ResultID }, new { @class = "btn btn-info buffer-left" })
+    }
+</p>
