2ae3654b7e247299e0bb80c56c3c499a9d7d6f2b
Leah Loversky <ldloversky@gmail.com>
2020-09-04 10:24:27 -0700

    Issue 50: Add financials summary chart to results page

---
+++ DervetApp/src/renderer/util/chart.js
@@ -0,0 +1,13 @@
+import { max } from 'lodash';
+import numeral from 'numeral';
+
+export const formatYAxisCurrency = (tick, index, ticks) => {
+  // If the maximum tick value is >= 1 million then divide all tick marks by 1mm
+  // and show one decimal point
+  if (max(ticks) >= 1000000) {
+    return `${numeral(tick / 1000000).format('$0,0')}m`;
+  }
+  return numeral(tick).format('$0,0');
+};
+
+export const formatYAxis = tick => numeral(tick).format('0,0.[000]');
