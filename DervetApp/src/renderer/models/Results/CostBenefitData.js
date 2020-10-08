896b1e65ada2248ed91696d4f8a8788a674e7acb
phna001 <hnathwani@epri.com>
2020-10-07 18:07:55 -0700

    cost benefit data passes tests successfully

--- DervetApp/src/renderer/models/Results/CostBenefitData.js
+++ DervetApp/src/renderer/models/Results/CostBenefitData.js
@@ -4,12 +4,15 @@ import BaseTableData from './BaseTableData';
 export class CostBenefitData extends BaseTableData {
   constructor(data) {
     super('cost_benefit.csv', data, true);
-    this.essBaseColor = tinycolor('#326581');
+    this.essBaseColor = tinycolor('blue');
+    this.essUsedColors = [];
     this.essTypes = ['battery'];
     this.intermittentResourceType = ['pv'];
-    this.intermittentResourceBaseColor = tinycolor('#807019');
+    this.intermittentResourceBaseColor = tinycolor('yellow');
+    this.pvUsedColors = [];
     this.generatorTypes = ['diesel', 'ice'];
-    this.generatorBaseColor = tinycolor('#393939');
+    this.generatorBaseColor = tinycolor('grey');
+    this.generatorUsedColors = [];
   }
   summaryData() {
     // look for row with 'Lieftime Present Value' in first index and return rest of row
@@ -37,16 +40,18 @@ export class CostBenefitData extends BaseTableData {
           name: valueStream,
           type: 'bar',
         };
-        // TODO add color if technology
+        // add color if technology
+        // assumes all DER rows are grouped by name
         // if (valueStream.indexOf(':') !== -1) {
         //   const derTag = valueStream.split(':')[0].toLowerCase();
-        //   // TODO select color based on tag grouping TODO
-        //   // TODO select color based on name TODO
-        //   // TODO select color base on valuestream TODO
-        // }
+        //   // TODO select BASE color based on tag grouping
 
+        //   // TODO SPIN base color for each unique name
+        //   // TODO change darkness of base color for valuestream of given name
+        //   traceTemplate.marker = { color: valuestreamColor };
+        // }
         // add trace to list
-        traces.pop(traceTemplate);
+        traces.push(traceTemplate);
       }
       rowNum += 1;
     }
@@ -55,17 +60,21 @@ export class CostBenefitData extends BaseTableData {
 }
 
 export const costBenefitArrayData = [
-  ['', 'Cost ($)', 'Benefit ($)'],
+  ['', 'Costs ($)', 'Benefit ($)'],
   ['Lifetime Present Value', 210000000, 390000000],
-  ['BATTERY: ess1 Capital Cost', 2e5, 0],
-  ['BATTERY: ess1 Fixed O&M', 4e5, 0],
-  ['BATTERY: ess1 Variable O&M', 3e5, 0],
-  ['PV: rooftop Capital Cost', 9e5, 0],
-  ['PV: rooftop Fixed O&M', 4e5, 0],
-  ['ICE: gen set Capital Cost', 3e5, 0],
-  ['ICE: gen set Fixed O&M', 8e5, 0],
-  ['ICE: gen set Variable O&M', 1e5, 0],
-  ['ICE: gen set Fuel', 4e5, 0],
+  ['BATTERY: ess1 - Capital Cost', 2e5, 0],
+  ['BATTERY: ess1 - Fixed O&M', 4e5, 0],
+  ['BATTERY: ess1 - Variable O&M', 3e5, 0],
+  ['PV: rooftop - Capital Cost', 9e5, 0],
+  ['PV: rooftop - Fixed O&M', 4e5, 0],
+  ['ICE: gen set - Capital Cost', 3e5, 0],
+  ['ICE: gen set - Fixed O&M', 8e5, 0],
+  ['ICE: gen set - Variable O&M', 1e5, 0],
+  ['ICE: gen set - Fuel', 4e5, 0],
+  // ['ICE: gen set1 - Capital Cost', 3e5, 0],
+  // ['ICE: gen set1 - Fixed O&M', 8e5, 0],
+  // ['ICE: gen set1 - Variable O&M', 1e5, 0],
+  // ['ICE: gen set1 - Fuel', 4e5, 0],
   ['Avoided Demand Charge', 0, 6e5],
   ['Avoided Energy Charge', 0, 7e5],
   ['Spinning Reserves', 0, 9e5],
@@ -79,63 +88,91 @@ export const costBenefitTraces = [
   {
     x: costBenefitXAxis,
     y: [2e5, 0],
-    name: 'BATTERY: ess1 Capital Cost',
+    name: 'BATTERY: ess1 - Capital Cost',
     marker: { color: '#326581' },
     type: 'bar',
   },
   {
     x: costBenefitXAxis,
     y: [4e5, 0],
-    name: 'BATTERY: ess1 Fixed O&M',
+    name: 'BATTERY: ess1 - Fixed O&M',
     marker: { color: '#6ca6c6' },
     type: 'bar',
   },
   {
     x: costBenefitXAxis,
     y: [3e5, 0],
-    name: 'BATTERY: ess1 Variable O&M',
+    name: 'BATTERY: ess1 - Variable O&M',
     marker: { color: '#a2c7db' },
     type: 'bar',
   },
   {
     x: costBenefitXAxis,
     y: [9e5, 0],
-    name: 'PV: rooftop Capital Cost',
+    name: 'PV: rooftop - Capital Cost',
     marker: { color: '#807019' },
     type: 'bar',
   },
   {
     x: costBenefitXAxis,
     y: [4e5, 0],
-    name: 'PV: rooftop Fixed O&M',
+    name: 'PV: rooftop - Fixed O&M',
     marker: { color: '#c0a926' },
     type: 'bar',
   },
   {
     x: costBenefitXAxis,
     y: [3e5, 0],
-    name: 'ICE: gen set Capital Cost',
+    name: 'ICE: gen set - Capital Cost',
     marker: { color: '#393939' },
     type: 'bar',
   },
   {
     x: costBenefitXAxis,
     y: [8e5, 0],
-    name: 'ICE: gen set Fixed O&M',
+    name: 'ICE: gen set - Fixed O&M',
     marker: { color: '#6a6a6a' },
     type: 'bar',
   },
   {
     x: costBenefitXAxis,
     y: [1e5, 0],
-    name: 'ICE: gen set Variable O&M',
+    name: 'ICE: gen set - Variable O&M',
     marker: { color: '#999999' },
     type: 'bar',
   },
   {
     x: costBenefitXAxis,
     y: [4e5, 0],
-    name: 'ICE: gen set Fuel',
+    name: 'ICE: gen set - Fuel',
+    marker: { color: '#bfbfbf' },
+    type: 'bar',
+  },
+  {
+    x: costBenefitXAxis,
+    y: [3e5, 0],
+    name: 'ICE: gen set1 - Capital Cost',
+    marker: { color: '#393939' },
+    type: 'bar',
+  },
+  {
+    x: costBenefitXAxis,
+    y: [8e5, 0],
+    name: 'ICE: gen set1 - Fixed O&M',
+    marker: { color: '#6a6a6a' },
+    type: 'bar',
+  },
+  {
+    x: costBenefitXAxis,
+    y: [1e5, 0],
+    name: 'ICE: gen set1 - Variable O&M',
+    marker: { color: '#999999' },
+    type: 'bar',
+  },
+  {
+    x: costBenefitXAxis,
+    y: [4e5, 0],
+    name: 'ICE: gen set1 - Fuel',
     marker: { color: '#bfbfbf' },
     type: 'bar',
   },
