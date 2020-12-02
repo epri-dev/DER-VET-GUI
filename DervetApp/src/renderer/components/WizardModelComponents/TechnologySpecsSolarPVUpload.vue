ccf23269cb3d8eb4d6605d9577a4c53dd0e1cb91
phna001 <hnathwani@epri.com>
2020-12-01 16:01:38 -0800

    pv gen white space is removed

--- DervetApp/src/renderer/components/WizardModelComponents/TechnologySpecsSolarPVUpload.vue
+++ DervetApp/src/renderer/components/WizardModelComponents/TechnologySpecsSolarPVUpload.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
-    <h3>Technology Specs: Solar PV Generation Profile Upload</h3>
-    <hr>
     <form class="form-horizontal form-buffer">
+      <h3>Technology Specs: Solar PV Generation Profile Upload</h3>
       <timeseries-data-upload
         chart-name="chartUploadedTimeSeries"
         data-name="solar generation profile"
@@ -29,7 +28,7 @@
           </div>
         </div>
       </div>
-      <br>
+
       <hr>
 
       <save-buttons
