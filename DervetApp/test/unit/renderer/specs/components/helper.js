a6dd0ab7beeb24d2e690338bd744ee3c9f0b1f14
Leah Loversky <ldloversky@gmail.com>
2020-12-21 14:12:33 -0600

    Issue 176: Inject vuelidate in unit test component mounting function

--- DervetApp/test/unit/renderer/specs/components/helper.js
+++ DervetApp/test/unit/renderer/specs/components/helper.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
+import Vuelidate from 'vuelidate';
 import router from '@/router';
 import store from '@/store';
+
+Vue.use(Vuelidate);
 
 const mountVueElement = (component, props) => (
   new Vue({
