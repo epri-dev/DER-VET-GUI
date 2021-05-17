4aa766bfa33552ad89d5905425c7544e0b1e03f6
phna001 <hnathwani@epri.com>
2021-05-17 09:04:25 -0700

    hardcoding python exe and run script location

--- DervetApp/src/main/service/dervet.js
+++ DervetApp/src/main/service/dervet.js
@@ -33,9 +33,13 @@ const getPythonExe = () => (
 
 const pythonExeExists = exePath => fs.existsSync(exePath);
 
-const getDevPythonRuntime = () => process.env.DERVET_PYTHON_RUNTIME;
+//const getDevPythonRuntime = () => process.env.DERVET_PYTHON_RUNTIME;
 
-const getDevPythonScript = () => process.env.DERVET_PYTHON_SCRIPT;
+//const getDevPythonScript = () => process.env.DERVET_PYTHON_SCRIPT;
+
+const getDevPythonRuntime = 'C:/ProgramData/Anaconda3/envs/dervet-venv/python.exe';
+
+const getDevPythonScript = 'C:/Users/phna001/Documents/dervet-gui/DervetBackEnd/dervet/run_DERVET.py';
 
 const spawnPackagedPythonProcess = (pythonExe, modelParametersPath) => (
   childProcess.execFile(pythonExe, [modelParametersPath])
